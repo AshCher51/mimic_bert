26d0f0b6ea5b3b242506e1a7d4887d2cce1bf1ae
Aashish Cheruvu <ashcher51@gmail.com>
2021-07-21 18:35:18 -0400

    save preprocessing and training outputs to disk

--- src/run.sh
+++ src/run.sh
@@ -1,2 +1,4 @@
-python preprocessing.py
-python actual_train.py --dev False --epochs 20
+nohup python preprocessing.py
+mv nohup.out preprocessing.out
+nohup python actual_train.py --dev False --epochs 20
+mv nohup.out training_output.out
